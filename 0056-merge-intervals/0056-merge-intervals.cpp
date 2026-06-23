bb7557711e8aaee23851c88a81086b314076868e
Dharma Malaviya <dharmamalaviya.18@gmail.com>
2026-06-23 11:39:52 +0530

    Time: 5 ms (51.64%), Space: 23.7 MB (95.33%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& inter) {
+        int n = inter.size();
+        sort(inter.begin(), inter.end());
+        vector<vector<int>> ans;
+        ans.push_back(inter[0]);
+        for(int i = 1; i<n; i++){
+           if(inter[i][0]  <= ans.back()[1]){
+            ans.back()[1] = max(ans.back()[1], inter[i][1]);
+           }
+           else{
+            ans.push_back(inter[i]);
+           }
+        }        
+        return ans;
+    }
+};
