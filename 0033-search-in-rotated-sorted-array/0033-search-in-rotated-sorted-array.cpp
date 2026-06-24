c9c3f4da4003963c5b7492d30d0ef91614cec059
Dharma Malaviya <dharmamalaviya.18@gmail.com>
2026-06-24 15:27:53 +0530

    Time: 0 ms (100%), Space: 15.2 MB (34.09%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int target) {
+        int n = arr.size();
+        int st = 0, end = n-1;
+
+        while(st <= end){
+            int mid = st + (end-st)/2;
+
+            if(arr[mid] == target){
+                return mid;
+            }
+
+            else if(arr[st] <= arr[mid]){     //left sorted
+                if(arr[st] <= target && target < arr[mid]){
+                    end = mid - 1;
+                }
+                else{
+                    st = mid + 1;
+                }
+            }
+
+            else{   //right sorted
+                if(arr[mid] < target && target <= arr[end]){
+                    st = mid + 1;
+                }
+                else{
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
