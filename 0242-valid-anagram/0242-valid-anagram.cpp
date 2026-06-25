3f0456c778f71e554d631c8b46eb5432f120aa7d
Dharma Malaviya <dharmamalaviya.18@gmail.com>
2026-06-25 22:24:40 +0530

    Time: 1 ms (54.68%), Space: 9.7 MB (49.41%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -5,13 +5,21 @@ public:
         if(m != n){
             return false;
         }
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if(s == t){
-            return true;
+        // Count frequency of each character in str1
+        int freq[26] = {0};
+        for(int i=0; i<m; i++){
+            freq[s[i] - 'a']++;
         }
-        else{
-            return false;
+        // Decrement frequency for each character in str2
+        for(int i=0; i<n; i++){
+            freq[t[i] - 'a']--;
         }
+        // Check if all frequencies are zero, meaning both strings have the same characters
+        for(int i=0; i<26; i++){
+            if(freq[i] != 0){
+                return false;
+            }
+        }
+        return true;
     }
 };
