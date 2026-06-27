c772e09038ad1ddd117eaca55f32fb1193f65f19
Dharma Malaviya <dharmamalaviya.18@gmail.com>
2026-06-27 19:30:06 +0530

    Create 118-PascalsTriangle

---
+++ 118-PascalsTriangle/118-PascalsTriangle.cpp
@@ -0,0 +1,16 @@
+// Last updated: 6/27/2026, 7:30:07 PM
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>> triangle;
+
+        for(int i=0; i<n; i++){
+            vector<int> row(i+1, 1);
+            for(int j=1; j<i; j++){
+                row[j] = triangle[i-1][j-1] + triangle[i-1][j];
+            }
+            triangle.push_back(row);
+        }
+    return triangle;
+    }
+};
