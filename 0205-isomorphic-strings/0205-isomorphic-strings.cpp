912778544614402e2bfc6c0d1683f296a7c829ba
Dharma Malaviya <dharmamalaviya.18@gmail.com>
2026-06-30 09:21:23 +0530

    Time: 0 ms (100%), Space: 9.3 MB (81.87%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -4,12 +4,16 @@ public:
         int n = s.size();
         if(n != t.size()) return false;
 
+        int ST[256] = {0};
+        int TS[256] = {0};
+
         for(int i=0; i<n; i++){
-            for(int j=0; j<t.size(); j++){
-                if((s[i] == s[j]) != (t[i] == t[j])){
-                    return false;
-                }
-            }
+            char a = s[i], b = t[i];
+            if(ST[a] && ST[a] != b) return false;
+            if(TS[b] && TS[b] != a) return false;
+
+            ST[a] = b;
+            TS[b] = a;
         }
         return true;
     }
