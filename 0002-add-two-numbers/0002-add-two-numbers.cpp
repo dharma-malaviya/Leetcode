ff19298928ba8806cb0b482965d3908337928837
Dharma Malaviya <dharmamalaviya.18@gmail.com>
2026-07-23 22:23:29 +0530

    Time: 0 ms (100%), Space: 77.4 MB (13.06%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -12,26 +12,32 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         string num1 = "", num2 = "";
-        while (l1) { num1 = char('0' + l1->val) + num1; l1 = l1->next; }
-        while (l2) { num2 = char('0' + l2->val) + num2; l2 = l2->next; }
+        while(l1){
+            num1 = char('0' + l1->val) + num1;
+            l1 = l1->next;
+        }
+        while(l2){
+            num2 = char('0' + l2->val) + num2;
+            l2 = l2->next;
+        }
 
-        // Big-number string addition
         string result = "";
-        int i = num1.size() - 1, j = num2.size() - 1, carry = 0;
-        while (i >= 0 || j >= 0 || carry) {
-            int d1 = (i >= 0) ? num1[i--] - '0' : 0;
-            int d2 = (j >= 0) ? num2[j--] - '0' : 0;
+        int i = num1.size()-1, j = num2.size()-1, carry = 0;
+        while(i>=0 || j>=0 || carry != 0){
+            int d1 = (i>=0) ? num1[i--] - '0' : 0;
+            int d2 = (j>=0) ? num2[j--] - '0' : 0;
             int sum = d1 + d2 + carry;
-            carry = sum / 10;
-            result += char('0' + sum % 10);
+
+            carry = sum/10;
+            result += char('0' + sum%10);
         }
 
-        ListNode dummy(0);
-        ListNode* tail = &dummy;
-        for (char c : result) {
-            tail->next = new ListNode(c - '0');
-            tail = tail->next;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+        for(char c : result){
+            temp->next = new ListNode(c - '0');
+            temp = temp->next;
         }
-        return dummy.next;
+        return dummy->next;
     }
 };
