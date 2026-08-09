03405fe549cecb570cd7377d1adad67f2e053cd4
Dharma Malaviya <dharmamalaviya.18@gmail.com>
2026-08-09 22:53:37 +0530

    Time: 11 ms (58.67%), Space: 14.2 MB (15.16%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        int result = nums[0]+nums[1]+nums[2];
+        sort(nums.begin(), nums.end());
         int n = nums.size();
+        int closet = nums[0] + nums[1] + nums[2];
+
         for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                for(int k=j+1; k<n; k++){
-                    int sum = nums[i]+nums[j]+nums[k];
-                    if(abs(sum-target) < abs(result-target)){
-                        result = sum;
-                    }
+            int left = i+1, right = n-1;
+            while(left<right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(abs(sum-target) < abs(closet-target)){
+                    closet = sum;
+                }
+
+                if(sum == target){
+                    return target;
+                }
+                else if(sum < target){
+                    left++;
+                }
+                else{
+                    right--;
                 }
             }
         }
-        return result;
+        return closet;
+
     }
 };
