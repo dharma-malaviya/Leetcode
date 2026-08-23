dbc7d6dc82acdc1102e2f2212f0d85a4fb782038
Dharma Malaviya <dharmamalaviya.18@gmail.com>
2026-08-23 21:38:27 +0530

    Time: 0 ms (100%), Space: 9 MB (32.32%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int carry = 0;
+        int i = a.size()-1, j = b.size()-1;
+        string result;
+
+        while(i>=0 || j>=0 || carry!=0){
+            int sum = carry;
+            if(i>=0) sum += a[i--] - '0';
+            if(j>=0) sum += b[j--] - '0';
+
+            result += (char)('0' + (sum%2));
+            carry = sum/2;
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
